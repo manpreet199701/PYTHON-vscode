f50522e9bbc2c3235e5e89c6a6d84aa996f1b5e5
Manpreet Kaur <manpreetkaur@Manpreets-MacBook-Air.local>
2025-03-20 03:24:40 +0530

    Create def.py

---
+++ def.py
@@ -0,0 +1,5 @@
+def greet_user(name, greeting="Hello"):
+    print(f"{greeting}, {name}!")
+
+greet_user("Alice")         # Using the default greeting ("Hello")
+greet_user("Bob", "Hi")     # Using a custom greeting ("Hi")
