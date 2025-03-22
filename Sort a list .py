495e4b39537b9bc00f5fb0280b19de710692efef
manpreet199701 <manpreetk1911@icloud.com>
2025-03-23 02:38:13 +0530

    Create Sort a list .py

---
+++ Sort a list .py
@@ -0,0 +1,8 @@
+#Sort a list in ascending and descending order.
+list_sort=[22.44,66,55,77,22,11]
+print("list before sorting",list_sort)
+list_sort.sort(reverse=True) 
+print("list after sorting in descending order:",list_sort) # descending order
+list_sort.sort()
+print("list after sorting in ascending order:",list_sort) # ascending order
+
