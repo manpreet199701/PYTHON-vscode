bb293d6b10eedefb8ac8819fb636b2ffec88a99b
manpreet199701 <manpreetk1911@icloud.com>
2025-03-23 02:34:09 +0530

    Create #Create a list with five elements and pr.py

---
+++ #Create a list with five elements and pr.py
@@ -0,0 +1,24 @@
+#Create a list with five elements and print each element using a loop.
+name = input("Enter your name: ")
+address = input("Enter your address: ")
+
+list_1= [name , address,]
+
+
+m
+phone = input("Enter your phone number: ")
+email = input("Enter your email: ")
+
+list_2= [phone , email]
+
+for i in list_1:
+    print(i)
+for i in list_2:
+    print(i)
+
+# Output: Enter your name: John
+
+
+
+        
+
