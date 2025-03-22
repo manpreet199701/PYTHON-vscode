1dd47bc89595e6a6297cdda306642fc611923dd2
manpreet199701 <manpreetk1911@icloud.com>
2025-03-23 02:34:34 +0530

    Create Create a dictionary.py

---
+++ Create a dictionary.py
@@ -0,0 +1,3 @@
+#Create a dictionary with keys as student names and values as their marks. Print the dictionary.
+stu_name = {'Sachin': 2, 'Rahul':9, 'Sourav':7, 'Anil':6}
+print("print results",stu_name)
