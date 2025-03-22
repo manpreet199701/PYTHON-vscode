fb595f5f43723e70d7ca1e92df8195cbf9821e82
manpreet199701 <manpreetk1911@icloud.com>
2025-03-23 02:37:23 +0530

    Create #Write a Python program that reads a tex.py

---
+++ #Write a Python program that reads a tex.py
@@ -0,0 +1,6 @@
+#Write a Python program that reads a text file and counts the number of lines and words in it.
+file_handle=open("no_of_sheets.py","r")
+count=1
+for cheese in file_handle:
+    count+=1
+print("line count:", count) 
