6f6cb25b4f64b7a4e59afd87441f6ce3ea697915
manpreet199701 <manpreetk1911@icloud.com>
2025-03-23 02:38:28 +0530

    Create sum and average list.py

---
+++ sum and average list.py
@@ -0,0 +1,21 @@
+# Find the sum and average of all numbers in a list.
+list_num=[22,22,111,111,000]
+sum_list=0
+av_list=0
+for i in list_num:
+    sum_list= sum(list_num)
+    av_list= sum_list / len(list_num)
+print("sum of list:",sum_list)
+print("average list:",av_list)
+
+
+
+
+
+
+
+
+
+
+
+
