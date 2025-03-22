786c6a8ff5175f1058a40f5e90a246cc86cfed58
manpreet199701 <manpreetk1911@icloud.com>
2025-03-23 02:37:50 +0530

    Create Append, insert, and remove in list.py

---
+++ Append, insert, and remove in list.py
@@ -0,0 +1,8 @@
+#Append, insert, and remove elements from a list.
+list_opp=[222,00,11,33,44,55,66,77,88,99]
+list_opp.append(2000) 
+print("print afert append:",list_opp)
+list_opp.insert(4,11111)
+print("list after insert:",list_opp)
+list_opp.remove(222)
+print("print after remove:",list_opp)
