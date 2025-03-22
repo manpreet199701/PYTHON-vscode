b371a0ae4c1dd7a46dff1e8e8c13c9c6d6033094
manpreet199701 <manpreetk1911@icloud.com>
2025-03-23 02:34:01 +0530

    Create # Create a function named calculate_area.py

---
+++ # Create a function named calculate_area.py
@@ -0,0 +1,13 @@
+# Create a function named calculate_area that computes the area of a rectangle. 
+# The function should accept two parameters: length and width. 
+# Demonstrate its usage by calculating the area for a rectangle with a length of 5 units and a width of 3 units.
+import math
+def calculate_area(lenght,breadth):
+    area = lenght * breadth
+    return area
+lenght= float(input("Enter the lenght of the rectangle: "))
+breadth= float(input("Enter the breadth of the rectangle: "))
+print("The area of the rectangle is: ", calculate_area(lenght,breadth))
+
+calculate_area(lenght ,breadth)
+
