e43a9b64d42e9b7182487569397c6ce33feeb194
manpreet199701 <manpreetk1911@icloud.com>
2025-03-23 02:34:23 +0530

    Create #Write a Python program to open a text f.py

---
+++ #Write a Python program to open a text f.py
@@ -0,0 +1,5 @@
+#Write a Python program to open a text file named sample.txt, read its contents, and print them to 
+# the console.
+file_handle=open('def attempts.py','r')
+print(file_handle.read())
+file_handle.close()
