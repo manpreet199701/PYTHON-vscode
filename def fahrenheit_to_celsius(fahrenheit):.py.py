15dcdb28d3930d33bbcec78d4ad78a970ff28808
manpreet199701 <manpreetk1911@icloud.com>
2025-03-23 16:59:42 +0530

    Delete def fahrenheit_to_celsius(fahrenheit):.py.py

--- def fahrenheit_to_celsius(fahrenheit):.py.py
+++
@@ -1,6 +0,0 @@
-def fahrenheit_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * 5/9
-    return celsius
-fahrenheit = float(input ("Enter the temperature in Fahrenheit: "))
-fahrenheit_to_celsius(fahrenheit)
-print("the value in celsius:",fahrenheit_to_celsius(fahrenheit))
