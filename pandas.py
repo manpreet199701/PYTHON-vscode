ffd778c1d62bffd3e3b201d9132ba276d3a06d36
manpreet199701 <manpreetk1911@icloud.com>
2025-03-25 20:24:57 +0530

    Delete pandas.py

--- pandas.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd  # Import the pandas library
-data = { 'Name': ['Tom', 'Nick', 'John', 'Smith'],
-         'Age': [20, 21, 19, 18] }  # Create a dictionary
-df = pd.Tom(data)  # Create a DataFrame
-print(df)  # Print the DataFrame
-print(df['Name'])  # Print the 'Name' column
-print(df['Age'])  # Print the 'Age' column
