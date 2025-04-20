d924137f5b2483d86bd8d781bc692b325a98da05
manpreet199701 <manpreetk1911@icloud.com>
2025-04-20 12:18:27 +0530

    Delete # Multiplication Table Program.py

--- # Multiplication Table Program.py
+++
@@ -1,9 +0,0 @@
-# Multiplication Table Program
-
-# Ask the user for a number
-num = int(input("Enter a number: "))
-
-# Print the multiplication table from 1 to 10
-print(f"Multiplication table for {num}:")
-for i in range(1, 11):
-    print(f"{num} x {i} = {num * i}")
