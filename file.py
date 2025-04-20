70bb2776a7b04546c58da7b9d7fe3bac4dca36d2
manpreet199701 <manpreetk1911@icloud.com>
2025-04-20 12:26:10 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,5 +1,6 @@
 #Write a Python program to open a text file named sample.txt, read its contents, and print them to 
 # the console.
-file_handle=open('def attempts.py','r')
-print(file_handle.read())
+file_handle=open('Frequency Counter.py') 
+print(file_handle.readline())
 file_handle.close()
+# The code opens a file named 'sample.txt' in read mode, reads its contents line by line, and prints each line to the console.
