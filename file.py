ff5cad9b4beedb71b35fc6c3a1c6d2f9b91c12a1
manpreet199701 <manpreetk1911@icloud.com>
2025-04-22 07:33:18 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,6 +1,15 @@
 #Write a Python program to open a text file named sample.txt, read its contents, and print them to 
 # the console.
-file_handle=open('Frequency Counter.py') 
-print(file_handle.readline())
+#file_handle=open('Frequency Counter.py')
+#count=1
+#for i in file_handle:
+ #   print(count, ":", i)
+  #  count = count + 1
+   # print(i)
+#file_handle.close() 
+file_handle = open('game.py')
+lines=file_handle.readlines()
+list(lines)
+for i in lines:
+    print(i)
 file_handle.close()
-# The code opens a file named 'sample.txt' in read mode, reads its contents line by line, and prints each line to the console.
